8a1731aaf70594433e43db529ae5bdd1be999f11
Tijs van Roon <knifter@gmail.com>
2021-08-02 16:32:25 +0200

    Updated axis_reader from Pavels version: AXI Width = AXIS width which removes two-compl support

--- axi_axis_reader_v1_0/src/axi_axis_reader.v
+++ axi_axis_reader_v1_0/src/axi_axis_reader.v
@@ -4,9 +4,7 @@
 module axi_axis_reader #
 (
   parameter integer AXI_DATA_WIDTH = 32,
-  parameter integer AXI_ADDR_WIDTH = 12,
-  parameter integer AXIS_DATA_WIDTH = 32,
-  parameter integer TWOS_COMPL = 1
+  parameter integer AXI_ADDR_WIDTH = 16
 )
 (
   // System signals
@@ -32,70 +30,57 @@ module axi_axis_reader #
   input  wire                      s_axi_rready,  // AXI4-Lite slave: Read data ready
 
   // Slave side
-  input  wire [AXIS_DATA_WIDTH-1:0] s_axis_tdata,
-  input  wire                      s_axis_tvalid,
-  output wire                      s_axis_tready
+  output wire                      s_axis_tready,
+  input  wire [AXI_DATA_WIDTH-1:0] s_axis_tdata,
+  input  wire                      s_axis_tvalid
 );
 
-    localparam SIGN_BIT = (1 << (AXIS_DATA_WIDTH - 1));
-    localparam NOSIGN_MASK = (SIGN_BIT - 1);
-   
-  wire [AXI_DATA_WIDTH-1:0] s_axis_tdata_sized;
-  // S_AXIS_DATA_WIDTH == S_AXI_DATA_WIDTH
-  if ( AXIS_DATA_WIDTH == AXI_DATA_WIDTH )  // -> [01234567]
-    assign s_axis_tdata_sized = s_axis_tdata;
-  if ( AXIS_DATA_WIDTH > AXI_DATA_WIDTH ) // -> [0123]4567
-    assign s_axis_tdata_sized = s_axis_tdata[AXIS_DATA_WIDTH-1:(AXIS_DATA_WIDTH-AXI_DATA_WIDTH)];
-  if ( AXIS_DATA_WIDTH < AXI_DATA_WIDTH ) // [<SIGN>/<0>][S123456789]
-  begin
-    if(TWOS_COMPL)
-        assign s_axis_tdata_sized = ((s_axis_tdata & NOSIGN_MASK) - (s_axis_tdata & SIGN_BIT));
-    else
-        assign s_axis_tdata_sized = { {(AXI_DATA_WIDTH - AXIS_DATA_WIDTH){1'b0}}, s_axis_tdata}; 
-  end
-
-  reg int_rvalid_reg, int_rvalid_next;
+  reg int_arready_reg, int_arready_next;
   reg [AXI_DATA_WIDTH-1:0] int_rdata_reg, int_rdata_next;
+  reg int_rvalid_reg, int_rvalid_next;
+
+  wire int_ardone_wire, int_rdone_wire;
+
   always @(posedge aclk)
   begin
     if(~aresetn)
     begin
-      int_rvalid_reg <= 1'b0;
+      int_arready_reg <= 1'b1;
       int_rdata_reg <= {(AXI_DATA_WIDTH){1'b0}};
+      int_rvalid_reg <= 1'b0;
     end
     else
     begin
-      int_rvalid_reg <= int_rvalid_next;
+      int_arready_reg <= int_arready_next;
       int_rdata_reg <= int_rdata_next;
+      int_rvalid_reg <= int_rvalid_next;
     end
   end
+
+  assign int_ardone_wire = ~int_arready_reg | s_axi_arvalid;
+  assign int_rdone_wire = ~int_rvalid_reg | s_axi_rready;
+
   always @*
   begin
-    int_rvalid_next = int_rvalid_reg;
+    int_arready_next = ~int_ardone_wire | int_rdone_wire;
     int_rdata_next = int_rdata_reg;
+    int_rvalid_next = ~int_rdone_wire | int_ardone_wire;
 
-    if(s_axi_arvalid)
+    if(int_ardone_wire & int_rdone_wire)
     begin
-      int_rvalid_next = 1'b1;
-      int_rdata_next = s_axis_tvalid ? s_axis_tdata_sized : {(AXI_DATA_WIDTH){1'b0}};            
-    end
-
-    if(s_axi_rready & int_rvalid_reg)
-    begin
-      int_rvalid_next = 1'b0;
+      int_rdata_next = s_axis_tvalid ? s_axis_tdata : {(AXI_DATA_WIDTH){1'b0}};
     end
   end
 
-  assign s_axi_rresp = 2'd0;
-
-  assign s_axi_arready = 1'b1;
+  assign s_axi_awready = 1'b0;
+  assign s_axi_wready = 1'b0;
+  assign s_axi_bresp = 2'd0;
+  assign s_axi_bvalid = 1'b0;
+  assign s_axi_arready = int_arready_reg;
   assign s_axi_rdata = int_rdata_reg;
+  assign s_axi_rresp = 2'd0;
   assign s_axi_rvalid = int_rvalid_reg;
 
-  assign s_axis_tready = s_axi_rready & int_rvalid_reg;
+  assign s_axis_tready = int_ardone_wire & int_rdone_wire;
 
-  assign s_axi_awready = 0;
-  assign s_axi_wready = 0;
-  assign s_axi_bresp = 2'b00;
-  assign s_axi_bvalid = 0;
 endmodule
--- axi_axis_reader_v1_0/xgui/axi_axis_reader_v1_1.tcl
+++ axi_axis_reader_v1_0/xgui/axi_axis_reader_v1_1.tcl
@@ -2,32 +2,18 @@
 proc init_gui { IPINST } {
   ipgui::add_param $IPINST -name "Component_Name"
   #Adding Page
-  set Page_0 [ipgui::add_page $IPINST -name "Page 0" -display_name {AXI}]
+  set Page_0 [ipgui::add_page $IPINST -name "Page 0" -display_name {Main}]
+  set_property tooltip {Main} ${Page_0}
   #Adding Group
   set AXI [ipgui::add_group $IPINST -name "AXI" -parent ${Page_0}]
   ipgui::add_param $IPINST -name "AXI_DATA_WIDTH" -parent ${AXI}
-  set_property tooltip {Width of the AXI data bus.} ${AXI_DATA_WIDTH}
   ipgui::add_param $IPINST -name "AXI_ADDR_WIDTH" -parent ${AXI}
-  set_property tooltip {Width of the AXI address bus.} ${AXI_ADDR_WIDTH}
-  set TWOS_COMPL [ipgui::add_param $IPINST -name "TWOS_COMPL" -parent ${AXI}]
-  set_property tooltip {AXIS Data will be sign-extended to AXI width} ${TWOS_COMPL}
 
   #Adding Group
-  set AXI-Stream [ipgui::add_group $IPINST -name "AXI-Stream" -parent ${Page_0}]
-  ipgui::add_param $IPINST -name "AXIS_DATA_WIDTH" -parent ${AXI-Stream} -widget comboBox
-  set_property tooltip {Width of the AXIS data bus} ${AXIS_DATA_WIDTH}
+  ipgui::add_group $IPINST -name "AXIS" -parent ${Page_0}
 
 
 
-}
-
-proc update_PARAM_VALUE.AXIS_DATA_WIDTH { PARAM_VALUE.AXIS_DATA_WIDTH } {
-	# Procedure called to update AXIS_DATA_WIDTH when any of the dependent parameters in the arguments change
-}
-
-proc validate_PARAM_VALUE.AXIS_DATA_WIDTH { PARAM_VALUE.AXIS_DATA_WIDTH } {
-	# Procedure called to validate AXIS_DATA_WIDTH
-	return true
 }
 
 proc update_PARAM_VALUE.AXI_ADDR_WIDTH { PARAM_VALUE.AXI_ADDR_WIDTH } {
@@ -48,15 +34,6 @@ proc validate_PARAM_VALUE.AXI_DATA_WIDTH { PARAM_VALUE.AXI_DATA_WIDTH } {
 	return true
 }
 
-proc update_PARAM_VALUE.TWOS_COMPL { PARAM_VALUE.TWOS_COMPL } {
-	# Procedure called to update TWOS_COMPL when any of the dependent parameters in the arguments change
-}
-
-proc validate_PARAM_VALUE.TWOS_COMPL { PARAM_VALUE.TWOS_COMPL } {
-	# Procedure called to validate TWOS_COMPL
-	return true
-}
-
 
 proc update_MODELPARAM_VALUE.AXI_DATA_WIDTH { MODELPARAM_VALUE.AXI_DATA_WIDTH PARAM_VALUE.AXI_DATA_WIDTH } {
 	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
@@ -66,15 +43,5 @@ proc update_MODELPARAM_VALUE.AXI_DATA_WIDTH { MODELPARAM_VALUE.AXI_DATA_WIDTH PA
 proc update_MODELPARAM_VALUE.AXI_ADDR_WIDTH { MODELPARAM_VALUE.AXI_ADDR_WIDTH PARAM_VALUE.AXI_ADDR_WIDTH } {
 	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
 	set_property value [get_property value ${PARAM_VALUE.AXI_ADDR_WIDTH}] ${MODELPARAM_VALUE.AXI_ADDR_WIDTH}
-}
-
-proc update_MODELPARAM_VALUE.AXIS_DATA_WIDTH { MODELPARAM_VALUE.AXIS_DATA_WIDTH PARAM_VALUE.AXIS_DATA_WIDTH } {
-	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
-	set_property value [get_property value ${PARAM_VALUE.AXIS_DATA_WIDTH}] ${MODELPARAM_VALUE.AXIS_DATA_WIDTH}
-}
-
-proc update_MODELPARAM_VALUE.TWOS_COMPL { MODELPARAM_VALUE.TWOS_COMPL PARAM_VALUE.TWOS_COMPL } {
-	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
-	set_property value [get_property value ${PARAM_VALUE.TWOS_COMPL}] ${MODELPARAM_VALUE.TWOS_COMPL}
 }
 
